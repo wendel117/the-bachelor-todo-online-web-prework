2088417b0c5b4bed0a87211de6ac63a1400a3f77
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-20 21:40:14 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,10 +1,20 @@
-def get_first_name_of_season_winner(data, season)
+#def get_first_name_of_season_winner(data, season)
   # code here
-  season_data = data[season]
-  winner_data = season_data.find{|contestant| contestant["status"] == "Winner"}
-  winner_first_name = winner_data["name"].split(" ").first
+#  season_data = data[season]
+#  winner_data = season_data.find{|contestant| contestant["status"] == "Winner"}
+#  winner_first_name = winner_data["name"].split(" ").first
+#end
+def get_first_name_of_season_winner(data, season)
+  winner = ""
+  data[season].each do |participant_hash| # participant hash is an array
+    participant_hash.select do |key, value|
+      if value == "Winner"
+        winner = participant_hash["name"].split(" ").first
+      end
+    end
+  end
+  winner
 end
-
 def get_contestant_name(data, occupation)
   # code here
 end
