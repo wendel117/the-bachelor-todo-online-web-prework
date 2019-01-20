acbabc89ce9b898d3ad38f08441bee50eea33cdc
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-20 21:38:31 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,8 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  season_data = data[season]
+  winner_data = season_data.find{|contestant| contestant["status"] == "Winner"}
+  winner_first_name = winner_data["name"].split(" ").first
 end
 
 def get_contestant_name(data, occupation)
